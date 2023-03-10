fc49be77e33b1b99f51d0b51fd4e48c5f088ce60
Anderson Mendes <anderssantos45@gmail.com>
2023-03-10 09:55:54 -0300

    First commit

---
+++ src/application/Main.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import exception.entities.DomainException;
+import model.entities.Account;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		System.out.println("Enter account data:");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double initialBalance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account account = new Account(number, holder, initialBalance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		
+		try {
+			account.withdraw(amount);
+			System.out.printf("New balance: %.2f%n",account.getBalance());
+		}
+		catch(DomainException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
+		
+		sc.close();
+
+	}
+	}
+
+}
